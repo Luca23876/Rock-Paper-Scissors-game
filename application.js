29d6bceab242ba822d20346c833c98b685e2683f
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2016-12-01 17:50:52 -0700

    Fixed errors

--- application.js
+++ application.js
@@ -1,104 +1,110 @@
 $(document).ready(function(){
-	$('.choiceOne').click(function(){
-		var rock = 0
-		var paper = 1
-		var scissors = 2
+	$('#choiceOne').click(function(){
+		var rock = 0;
+		var paper = 1;
+		var scissors = 2;
 		var playerChoice = rock;
 		var computerChoice = Math.random();
 		if(computerChoice < .34){
 			var computerFinal = rock;
-		}else if(computerChoice >= .34, computerChoice <= .66){
+		}else if(computerChoice >= .34 && computerChoice <= .66){
 			var computerFinal = paper;
 		}else{
 			var computerFinal = scissors;
-		};
+		}
 		if(playerChoice === computerFinal){
 			alert("It's a tie!");
-		}else if(playerChoice === rock, computerFinal === paper){
+		}
+		if(playerChoice === rock && computerFinal === paper){
 			alert("computer wins");
-		}else if(playerChoice === paper, computerFinal === scissors){
+		}
+		if(playerChoice === paper && computerFinal === scissors){
 			alert("computer wins");
-		}else if(playerChoice === scissors, computerFinal === rock){
+		}
+		if(playerChoice === scissors && computerFinal === rock){
 			alert("computer wins");
-		}else if(playerChoice === rock, computerFinal === scissors){
+		}
+		if(playerChoice === rock && computerFinal === scissors){
 			alert("You Win!");
-		}else if(playerChoice === paper, computerFinal === rock){
+		}
+		if(playerChoice === paper && computerFinal === rock){
 			alert("You Win!");
-		}else{
+		}
+		if(playerChoice === scissors && computerFinal === paper){
 			alert("You Win!");
-		};
+		}
 	});
-	$('.choiceTwo').click(function(){
-		var rock = 0
-		var paper = 1
-		var scissors = 2
+	$('#choiceTwo').click(function(){
+		var rock = 0;
+		var paper = 1;
+		var scissors = 2;
 		var playerChoice = paper;
 		var computerChoice = Math.random();
 		if(computerChoice < .34){
 			var computerFinal = rock;
-		}else if(computerChoice >= .34, computerChoice <= .66){
+		}else if(computerChoice >= .34 && computerChoice <= .66){
 			var computerFinal = paper;
 		}else{
 			var computerFinal = scissors;
-		};
+		}
 		if(playerChoice === computerFinal){
 			alert("It's a tie!");
-		};
+		}
 		if(playerChoice === computerFinal){
 			alert("It's a tie!");
-		};
-		if(playerChoice === rock, computerFinal === paper){
+		}
+		if(playerChoice === rock && computerFinal === paper){
 			alert("computer wins");
-		};
-		if(playerChoice === paper, computerFinal === scissors){
+		}
+		if(playerChoice === paper && computerFinal === scissors){
 			alert("computer wins");
-		};
-		if(playerChoice === scissors, computerFinal === rock){
+		}
+		if(playerChoice === scissors && computerFinal === rock){
 			alert("computer wins");
-		};
-		if(playerChoice === rock, computerFinal === scissors){
+		}
+		if(playerChoice === rock && computerFinal === scissors){
 			alert("You Win!");
-		};
-		if(playerChoice === paper, computerFinal === rock){
+		}
+		if(playerChoice === paper && computerFinal === rock){
 			alert("You Win!");
-		};
-		if(playerChoice === scissors, computerFinal === paper){
+		}
+		if(playerChoice === scissors && computerFinal === paper){
 			alert("You Win!");
-		};
+		}
 	});
-	$('.choiceThree').click(function(){
-		var rock = 0
-		var paper = 1
-		var scissors = 2
+	$('#choiceThree').click(function(){
+		var rock = 0;
+		var paper = 1;
+		var scissors = 2;
 		var playerChoice = scissors;
 		var computerChoice = Math.random();
 		if(computerChoice < .34){
 			var computerFinal = rock;
-		}else if(computerChoice >= .34, computerChoice <= .66){
+		}else if(computerChoice >= .34 && computerChoice <= .66){
 			var computerFinal = paper;
 		}else{
 			var computerFinal = scissors;
-		};
+		}
 		if(playerChoice === computerFinal){
 			alert("It's a tie!");
-		};
-		if(playerChoice === rock, computerFinal === paper){
+		}
+		if(playerChoice === rock && computerFinal === paper){
 			alert("computer wins");
-		};
-		if(playerChoice === paper, computerFinal === scissors){
+		}
+		if(playerChoice === paper && computerFinal === scissors){
 			alert("computer wins");
-		};
-		if(playerChoice === scissors, computerFinal === rock){
+		}
+		if(playerChoice === scissors && computerFinal === rock){
 			alert("computer wins");
-		};
-		if(playerChoice === rock, computerFinal === scissors){
+		}
+		if(playerChoice === rock && computerFinal === scissors){
 			alert("You Win!");
-		};
-		if(playerChoice === paper, computerFinal === rock){
+		}
+		if(playerChoice === paper && computerFinal === rock){
 			alert("You Win!");
-		};
-		if(playerChoice === scissors, computerFinal === paper){
+		}
+		if(playerChoice === scissors && computerFinal === paper){
 			alert("You Win!");
-		};
+		}
 	});
 });
